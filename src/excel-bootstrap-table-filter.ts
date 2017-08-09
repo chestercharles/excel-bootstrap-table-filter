9012032b80562d36d755f5445c9a10529c0a6f32
chester <chester.c.carmer@intel.com>
2017-08-09 09:30:00 -0700

    cast jquery.fn as any to suppress ts errors

--- src/excel-bootstrap-table-filter.ts
+++ src/excel-bootstrap-table-filter.ts
@@ -2,17 +2,17 @@ import $ from 'jquery';
 import { FilterCollection } from './FilterCollection'
 
 // Define the plugin function on the jQuery extension point.
-$.fn.excelTableFilter = function (this: JQuery, options: Options) {
+($.fn as any).excelTableFilter = function (this: JQuery, options: Options) {
   let target = this;
   // Merge the global options with the per-call options.
-  options = $.extend({}, $.fn.excelTableFilter.options, options);
+  options = $.extend({}, ($.fn as any).excelTableFilter.options, options);
   
   let filterCollection = new FilterCollection(target);
   filterCollection.initialize();
 
   // Return the jQuery object for chaining.
   return target;
-}
+};
 
 // Define the plugin's global default options.
-$.fn.excelTableFilter.options = {};
+($.fn as any).excelTableFilter.options = {};
